a536dd2a4f1fb1bb3dc0011709a386d1c7096787
Tom Stout <tom.stout325@gmail.com>
2021-03-19 21:33:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 
     const baseUrl = 'https://picsum.photos/v2/list'
 
-    var shutter = document.querySelector(".camera")
+    let shutter = document.querySelector('camera');
+    let button = document.createElement('button');
+   
    
    
         
      function takePic()  {
-         fetch(baseUrl)
+        fetch(baseUrl)
              .then (res => res.json())
              .then (json => {
                  console.log(json)    
                  let img = document.getElementById('image')
                  img.src = json[20].download_url;
-                
               })
-     }
+       
+              button.innerHTML = "Want more inspiration?  Click here!";                   
+              document.body.appendChild(button);               
+    }
 
      camera.addEventListener("click", takePic);
+
+     
+    function refresh() {
+        let img = document.getElementById('image')
+        img.src=("https://picsum.photos/916/610?random=7")
+  
+     }
+
+     button.addEventListener("click", refresh)
+
